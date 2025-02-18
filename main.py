8b95c8eb6102ba4a249ad0658184927c656abd91
CasperD598 <casper.20@hotmail.dk>
2025-02-18 08:20:21 +0100

    v0.2 added FPS limit

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ from constants import *
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
+        #Makes it possible to close screen using the "X" button.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
-        pygame.Surface.fill(screen,(0,0,0))
+
+        screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
